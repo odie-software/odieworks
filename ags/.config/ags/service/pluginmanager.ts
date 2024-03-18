f93985a1e993ad229862f00c791a890eacf55380
hdev1 <husseinaljazairi@gmail.com>
2024-03-18 10:30:10 +0100

    feat(pluginmanager): load manifest

--- ags/.config/ags/service/pluginmanager.ts
+++ ags/.config/ags/service/pluginmanager.ts
@@ -1,4 +1,5 @@
 import { listDir } from "../lib/utils"
+const PLUGIN_DIR = `${App.configDir}/plugins`
 
 print('plugin manager init')
 class PluginManager extends Service {
@@ -10,13 +11,22 @@ class PluginManager extends Service {
 
     plugins = []
 
-    async getPlugins() {
-        try {
-            this.plugins = await listDir(`${App.configDir}/plugins`)
-        } catch(err) {
-            print(err)
+    async getPlugins() {        
+        let pluginDirs = await listDir(PLUGIN_DIR)
+        this.plugins = []
+
+        for (let file of pluginDirs) {
+            try {
+                let manifest = await Utils.readFileAsync(`${PLUGIN_DIR}/${file}/manifest.json`)
+                manifest = JSON.parse(manifest)
+                this.plugins.push(manifest)
+                print(`Loaded manifest for plugin ${file}: '${manifest.name}'`)
+                // TODO: check AGS version
+                
+            } catch(err) {
+                print(`Error loading plugin ${file}: ${err}`)
+            }
         }
-        print(this.plugins)
     }
 
     activateHook() {
